2b84d63d6613695fafe76740652098dbfb90755a
KanyeWestLovers <81704481+KanyeWestLovers@users.noreply.github.com>
2024-05-15 06:57:40 +0200

    Update graphLevel1.h

--- S2/graphLevel1.h
+++ S2/graphLevel1.h
@@ -1,35 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include "raylib.h"
 
 #ifndef GRAPH_LEVEL_1_INCLUDED
 #define GRAPH_LEVEL_1_INCLUDED
 #define NB_MONSTER 16
+#define NB_EDGE 45
 
-typedef struct Monster Monster;
-typedef struct GraphMonster GraphMonster;
+typedef struct Graph Graph;
 typedef struct ListMonster ListMonster;
+typedef struct Arc Arc;
 
 
-struct GraphMonster
+/*struct GraphMonster
 {
     //int nextvertex;
     Monster* monsterType; //weight
     
-};
+};*/
 
-struct Monster
+struct Graph
 {
     int **adjency;
-    int monsterType;
-    int nextvertex;
-    Monster* next;
+    Arc *arrayArc;
+    Graph* next;
+    int tv;
+    int te;
+};
+
+struct Arc
+{
+    int u;
+    int v;
+    int w;
 };
 
 struct ListMonster
 {
-    Monster* head;
-    int nbVertex;
+    Graph* head;
 };
 
 
