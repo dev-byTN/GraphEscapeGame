77400870d380837a57c05f1cbdc84f076cb5f9da
KanyeWestLovers <81704481+KanyeWestLovers@users.noreply.github.com>
2024-05-27 22:34:00 +0200

    Delete GraphViewer directory

--- GraphViewer/json/readJson.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "json.h"
-
-typed(json_element) interpretJSon(const char* fileName);
-
-typed(json_element) getJsonElementFromName(typed(json_element) element, const char* nodeName);
-
-bool hasElementWithName(typed(json_element) element, const char* nodeName);
-
